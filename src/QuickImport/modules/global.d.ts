4b94b37eca43fe5c087115dbb1adb1e1a6b44c8a
ZoruaFox <zorua@vip.qq.com>
2024-06-24 02:58:45 +0800

    revert: quickImport

--- src/QuickImport/modules/global.d.ts
+++
@@ -1,8 +0,0 @@
-type RefreshPage = import('./core').RefreshPage;
-type DetectIfFileRedirect = import('./core').DetectIfFileRedirect;
-
-declare module 'ext.gadget.QuickImport' {
-	export const api: mw.Api;
-	export const refreshPage: RefreshPage;
-	export const detectIfFileRedirect: DetectIfFileRedirect;
-}
