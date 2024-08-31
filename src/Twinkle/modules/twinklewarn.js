948a317aa488993c9bd0088eaee397d4b19c1032
海屿有燕 <zorua@vip.qq.com>
2024-09-01 01:09:45 +0800

    fix(Twinkle): 移动头像违规至警告

--- src/Twinkle/modules/twinklewarn.js
+++ src/Twinkle/modules/twinklewarn.js
@@ -719,10 +719,6 @@
 				label: window.wgULS('在编辑摘要制造不适当的内容', '在編輯摘要製造不適當的內容'),
 				summary: window.wgULS('警告：在编辑摘要制造不适当的内容', '警告：在編輯摘要製造不適當的內容'),
 			},
-			'uw-illegalavatar': {
-				label: window.wgULS('头像违规', '頭像違規'),
-				summary: window.wgULS('您近期上传的头像违规', '您近期上傳的頭像違規'),
-			},
 		},
 		singlewarn: {
 			'uw-3rr': {
@@ -748,6 +744,10 @@
 			'uw-pinfo': {
 				label: window.wgULS('张贴他人隐私', '張貼他人隱私'),
 				summary: window.wgULS('警告：张贴他人隐私', '警告：張貼他人隱私'),
+			},
+			'uw-illegalavatar': {
+				label: window.wgULS('头像违规', '頭像違規'),
+				summary: window.wgULS('您近期上传的头像违规', '您近期上傳的頭像違規'),
 			},
 		},
 	};
